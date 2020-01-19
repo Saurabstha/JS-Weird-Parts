e3afc24c119b6ff8fd30472abaf38f6bc22b959e
unknown <saurab.shrestha99@gmail.com>
2020-01-19 21:47:06 +0545

    upto 3 types and operators

---
+++ 3 Types and Operators/027 existence and boolean/app.js
@@ -0,0 +1,9 @@
+var a;
+
+// goes to internet and looks for a value
+
+if (a) {
+  console.log('Something is there.');  
+}
+
+// output is blank because a is undefined and it is coerced(converted to false)
---
+++ 3 Types and Operators/028 default values/app.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    name = name || 'Saurab'; //Saurab is default name here
+    console.log('Hello ' + name);    
+}
+
+greet('Tony');
+greet();
